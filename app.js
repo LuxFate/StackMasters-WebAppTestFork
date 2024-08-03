6c85b4a21a610e41596a009808b535a3fd480182
Michael <michaelmathebula009@gmail.com>
2024-08-04 00:23:18 +0200

    made a .env file and connected to local mysql database

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+//dotenv is what we will use to store passwords basically, hence .env
+dotenv.config({path: './.env'})
 
 const app = express();
+
+const db = mysql.createConnection({
+//IMP: i can put ip address of cloud server here when its time to move to cloud
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database:'stackmasters_login'
+});
+
+//connecting to the db
+db.connect ( (error) => {
+    if(error) {
+        console.log(error)
+    } else{
+        console.log("MYSQL DB Connected!")
+    }
+})
 
 //this is a get request
 //req is request. res is respond
