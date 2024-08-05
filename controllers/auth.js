fc830489fe239c9bb35e972c7d279070d6e64122
Michael <michaelmathebula009@gmail.com>
2024-08-05 13:53:59 +0200

    added login page and logini dependency

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const db = mysql.createConnection({
     //IMP: i can put ip address of cloud server here when its time to move to cloud
@@ -15,24 +17,39 @@ exports.register =(req, res) =>
 
     const {name,  email, password, passwordConfirm} = req.body;
 
-    db.query('SELECT email FROM users WHERE email =?', [email], (error, results) =>
+    db.query('SELECT email FROM users WHERE email =?', [email], async (error, results) =>
     {
         if(error){
             console.log(error);
         }
         if(results.length> 0){
             return res.render('register', {
-                message: "That email has been taken"
+                message: "Existing user, try different details"
             })
         }
         else if(password !== passwordConfirm){
             return res.render('register', {
                 message: "passwords do not match"
-            })
+            });
         }
 
-    });
+        //passsword will has password 8 times
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
 
-    res.send("Form submitted");
+        //adding user into database with secure hashed password
+        db.query("INSERT INTO users SET ?", {name: name,email: email,password: hashedPassword}, (error, results) => {
+            if(error){
+                console.log(error);
+            }
+            else
+            {
+                console.log(results);
+                return res.render('register', {
+                    message: "User is registered"
+                });
+            }
+        })
+    });
 
 }
--- routes/pages.js
+++ routes/pages.js
@@ -15,4 +15,9 @@ router.get("/register", (req, res) => {
     res.render("register");
 });
 
+
+//login page
+router.get("/login", (req, res) => {
+    res.render("login");
+});
 module.exports = router;
