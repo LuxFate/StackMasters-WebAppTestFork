d97d422fc71a531006b7c6848146c30ef06acf25
Michael <michaelmathebula009@gmail.com>
2024-08-05 12:53:42 +0200

    added new dependencies called bcryptjs and cookie-parser jsonwebtoken

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,38 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    //IMP: i can put ip address of cloud server here when its time to move to cloud
+        host: process.env.DATABSE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database:process.env.DATABASE
+    });
+
+
 exports.register =(req, res) =>
 {
     console.log(req.body);
-    res.send("form submitted");
+
+    const {name,  email, password, passwordConfirm} = req.body;
+
+    db.query('SELECT email FROM users WHERE email =?', [email], (error, results) =>
+    {
+        if(error){
+            console.log(error);
+        }
+        if(results.length> 0){
+            return res.render('register', {
+                message: "That email has been taken"
+            })
+        }
+        else if(password !== passwordConfirm){
+            return res.render('register', {
+                message: "passwords do not match"
+            })
+        }
+
+    });
+
+    res.send("Form submitted");
 
 }
