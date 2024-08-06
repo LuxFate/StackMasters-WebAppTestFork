fb22dd322de0352a5271fbd70f45a242892ec0be
Michael <michaelmathebula009@gmail.com>
2024-08-06 13:38:30 +0200

    under auth, exports.register and login are able to encyrpt password via hashing and login compares hash password, if it matches. it will allow user to login

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 const db = mysql.createConnection({
 //IMP: i can put ip address of cloud server here when its time to move to cloud
-    host: process.env.DATABSE_HOST,
+    host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database:process.env.DATABASE
