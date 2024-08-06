cc52b575a5a8f2c8e10fb440d3efffaba748918d
LuxFate <143355319+LuxFate@users.noreply.github.com>
2024-08-06 11:54:26 +0200

    Added comments

--- controllers/assignmentController.js
+++ controllers/assignmentController.js
@@ -7,10 +7,11 @@ const db = mysql.createConnection({
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
 });
-
+//Defines the function which is exported and used as a route handler
 exports.createAssignment = (req, res) => {
+    //This logs the data sent by the client
     console.log(req.body);
-
+    //this extracts specific fields from the req.body
     const {
         assignmentID,
         Module,
@@ -19,7 +20,7 @@ exports.createAssignment = (req, res) => {
         dueDate,
         assignmentInfo
     } = req.body;
-
+    //this executes the sql query using client values
     db.query('INSERT INTO assignments SET ?', {
         assignmentID : assignmentID,
         Module: Module,
@@ -28,85 +29,100 @@ exports.createAssignment = (req, res) => {
         dueDate: dueDate,
         assignmentInfo: assignmentInfo
     }, (err, results) => {
+        //error handaling by checking if there is a error
         if (err) {
             console.log(err);
+            //This sends a message to client that error has occured
             return res.render('assignments', {
                 message: "Error occurred."
             });
         }else {
             console.log(results);
+            //this displays to the client there was no error
             return res.render('assignments', {
                 message: "Assignment created"
             });
         }
     });
 };
-
+//Defines the function which is exported and used as a route handler
 exports.getAssignment = (req, res) => {
+    //This logs the data sent by the client
     console.log(req.body);
-
+    //this extracts specific fields from the req.body
     const {
         assignmentID,
     } = req.body;
-
+    //this executes the sql query using client values
     db.query('SELECT * FROM assignmnets WHERE assignmentID =?', 
         [assignmentID], (err, results) => {
+            //error handaling by checking if there is a error
             if(err){
                 console.log(err);
+                //This sends a message to client that error has occured
                 return res.render('assignments', {
                     message: "Error occurred."
                 });
             }else{
                 console.log(results);
+                //this displays to the client there was no error
                 return res.render('assignments', {
                     message: "Assignment viewed"
                 });
             }
         });
 }
-
+//Defines the function which is exported and used as a route handler
 exports.updateAssignment = (req, res) => {
+    //This logs the data sent by the client
     console.log(req.body);
-
+    //this extracts specific fields from the req.body
     const{
         Module,
         assignmentName,
         dueDate,
         assignmentInfo
     } = req.body;
-
+    //this executes the sql query using client values
     db.query('UPDATE * WHERE assignmentID ?', 
        [assignmentID], (err, results) => {
+        //error handaling by checking if there is a error
         if(err){
             console.log(err);
+            //This sends a message to client that error has occured
             return res.render('assignments',{
                 message: "Error occured."
             });
         }else{
             console.log(results);
+            //this displays to the client there was no error
             return res.render('assignments', {
                 message: "View assignment"
             });
         }
     });
 }
-
+//Defines the function which is exported and used as a route handler
 exports.deleteAssignment = (req, res) => {
+    //This logs the data sent by the client
     console.log(req.body);
-
+    //this extracts specific fields from the req.body
     const{
         assignmentID,
     } = req.body;
-
+    //this executes the sql query using client values
     db.query('DELETE FROM assignment WHERE ?',
         [assignmentID], (err, results) => {
+            //error handaling by checking if there is a error
             if(err){
                 console.log(err);
+                //This sends a message to client that error has occured
                 return res.render('assignment', {
                     message: "Error occured."
                 });
             }else{
                 console.log(results);
+                //this displays to the client there was no error
                 return res.render('assignment', {
                     message: "Deleted assignment"
                 });
--- routes/assignmentsRoutes.js
+++ routes/assignmentsRoutes.js
@@ -1,6 +1,7 @@
+//Routes for assignments will be hosted using express router
 const router = express.Router();
 const express = require("express");
-
+//This is the specific functions that are coded in the controller
 const{
     createAssignment,
     getAssignment,
@@ -8,9 +9,14 @@ const{
     deleteAssignment
 } = require('./controllers/assignmentController');
 
+//This is a request to this route path to executed
+//This is used to create a new assignment 
 router.post('/', createAssignment);
+//This is used to retrieve assignment
 router.get('/', getAssignment);
+//this is used to replace data
 router.put('/', updateAssignment);
+//this is used to remove data
 router.delete('/', deleteAssignment);
-
+//This is to export the router
 module.exports = router;
