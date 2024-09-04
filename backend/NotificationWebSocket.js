043ea84408c9495b44ba7ef001f7318c6fde1a28
Michael <michaelmathebula009@gmail.com>
2024-09-04 11:31:33 +0200

    modified code, because it gave some server error when trying to test api. it works now. updated database connection code

--- backend/NotificationWebSocket.js
+++ backend/NotificationWebSocket.js
@@ -1,35 +1,6 @@
 const Assignment = require('./models/assignment');
 //const User = require('../controllers/user');
-const http = require('http');
-const socketIo = require('socket.io');
 
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Socket.IO server running');
-});
-
-const io = socketIo(server);
-
-// Set up Socket.IO
-io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-    });
-});
-
-// Function to emit notifications
-const emitNotification = (event, data) => {
-    io.emit(event, data);
-};
-
-// Export the function to emit notifications
-module.exports = { io, emitNotification };
-
-// Start the server
-server.listen(8000, () => console.log("listening on http://localhost:8000"));
 module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log('user connected');
@@ -95,3 +66,33 @@ module.exports = (io) => {
         });
     });
 }
+const http = require('http');
+const socketIo = require('socket.io');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Socket.IO server running');
+});
+
+const io = socketIo(server);
+
+// Set up Socket.IO
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+// Function to emit notifications
+const emitNotification = (event, data) => {
+    io.emit(event, data);
+};
+
+// Export the function to emit notifications
+module.exports = { io, emitNotification };
+
+// Start the server
+server.listen(8000, () => console.log("listening on http://localhost:8000"));
